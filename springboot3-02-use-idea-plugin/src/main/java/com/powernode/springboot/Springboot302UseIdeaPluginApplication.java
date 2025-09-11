0c5f2dac95705f635f463a1ec22ff2d6ebd4ca04
whitefox <dongfz@foxmail.com>
2025-09-11 23:04:27 +0800

    出了点问题，差点翻车。Maven突然没有权限新建仓库中的内容了，重启了一下IDEA好了

---
+++ springboot3-02-use-idea-plugin/src/main/java/com/powernode/springboot/Springboot302UseIdeaPluginApplication.java
@@ -0,0 +1,13 @@
+package com.powernode.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot302UseIdeaPluginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot302UseIdeaPluginApplication.class, args);
+    }
+
+}
